49b34d5c6ddca07363226fafb12587722d342df6
Max Strange <stranm1@spu.edu>
2015-12-24 12:31:10 -0800

    Wrote a first draft of the uart code
    Doesn't work. :(

--- UART/main.c
+++ UART/main.c
@@ -3,16 +3,22 @@
 #include <stdio.h>
 #include "nrf_delay.h"
 #include "my_led.h"
+#include "my_uart.h"
 
 
 int main(void)
 {
     led_init();
+    uart_init();
+
     while (true)
     {
       led_blink_all();
       nrf_delay_ms(500);
       led_pattern_dance();
       nrf_delay_ms(500);
+      led_pattern_dance();
+      nrf_delay_ms(500);
+      uart_write_str("hello");
     }
 }
--- UART/my_uart.c
+++ UART/my_uart.c
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "my_uart.h"
+#include "nrf.h"
+
+
+static bool uart_is_on = false;
+
+
 
 void uart_init(void)
 {
@@ -30,4 +36,46 @@ void uart_init(void)
   On this kit, the RX pin is P0.11 and
   the TX pin is P0.09.
   */
+  NRF_GPIO->OUTSET |= TXPIN;  //set the tx pin to high for some reason
+
+  NRF_GPIO->TXPINCFG |= 1; //set tx dir to out
+  NRF_GPIO->RXPINCFG &= ~(1 << 1);  //connect rx to input buffer
+  NRF_GPIO->RXPINCFG |= (1 << 2) | (1 << 3); //enable the pull-up on rx
+
+  NRF_UART0->PSELTXD |= TXPIN;
+  NRF_UART0->PSELRXD |= RXPIN;
+
+  /*
+  Configure the baud rate.
+  */
+  NRF_UART0->BAUDRATE = 0x00275000; //set to 9600 baud
+
+  /*
+  Default settings for parity and flow control are fine (no partiy bit,
+  no flow control)
+  */
+
+  /*
+  Ignore interrupts for now
+  */
+}
+
+void uart_write_str(const char *str)
+{
+  //if the tx is not on, turn it on
+  if (!uart_is_on)
+  {
+    NRF_UART0->TASKS_STARTTX = 1;
+    NRF_UART0->TASKS_STARTRX = 1;
+    uart_is_on = true;
+  }
+
+  while (*str != '\0')
+  {
+    //load the next char into the tx buffer
+    NRF_UART0->TXD = *str++;
+
+    while (NRF_UART0->EVENTS_TXDRDY == 0)
+      ;//block until ready
+  }
 }
